db816199a4142eb69701f962987d2bc9fbf5a583
ulloacastillo <joulloa@lab.ztz.ai>
2024-04-06 23:23:28 -0300

    server to static astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: vercel()
 })
